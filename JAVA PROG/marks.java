84d3591c35ea4f9aedf379e630230accd38b895a
Rashviha Murugesan <rashvihamurugesan@gmail.com>
2026-01-10 10:41:11 +0530

    Add files via upload

---
+++ JAVA PROG/marks.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+public class marks{
+	//return type methodName(){code}
+	public static String details(int Mark, String name){
+		return Mark+""+name;
+	}
+	public static void main (String[] args){
+		Scanner ref = new Scanner(System.in);
+		System.out.println(details(30, "java"));
+		}
+	}
